c03ce99c30a1c839c1ff9a5c09fc484d20ece740
Vikas Shivappa <vikas.shivappa@linux.intel.com>
2016-05-18 15:02:22 -0700

    x86/intel_rdt: Implement scheduling support for Intel RDT
    Adds support for IA32_PQR_ASSOC MSR writes during task scheduling. For
    Cache Allocation, MSR write would let the task fill in the cache
    'subset' represented by the task's capacity bit mask.
    
    The high 32 bits in the per processor MSR IA32_PQR_ASSOC represents the
    CLOSid. During context switch kernel implements this by writing the
    CLOSid of the task belongs to the CPU's IA32_PQR_ASSOC MSR.
    
    This patch also implements a common software cache for IA32_PQR_MSR
    (RMID 0:9, CLOSId 32:63) to be used by both Cache monitoring (CMT) and
    Cache allocation. CMT updates the RMID where as cache_alloc updates the
    CLOSid in the software cache. During scheduling when the new RMID/CLOSid
    value is different from the cached values, IA32_PQR_MSR is updated.
    Since the measured rdmsr latency for IA32_PQR_MSR is very high (~250
     cycles) this software cache is necessary to avoid reading the MSR to
    compare the current CLOSid value.
    
    The following considerations are done for the PQR MSR write so that it
    minimally impacts scheduler hot path:
     - This path does not exist on any non-intel platforms.
     - On Intel platforms, this would not exist by default unless INTEL_RDT
     is enabled.
     - remains a no-op when INTEL_RDT is enabled and intel SKU does not
     support the feature.
     - When feature is available and enabled, never does MSR write till the
     user manually starts using one of the capacity bit masks.
     - MSR write is only done when there is a task with different Closid is
     scheduled on the CPU. Typically if the task groups are bound to be
     scheduled on a set of CPUs, the number of MSR writes is greatly
     reduced.
     - A per CPU cache of CLOSids is maintained to do the check so that we
     don't have to do a rdmsr which actually costs a lot of cycles.
    
    Signed-off-by: Vikas Shivappa <vikas.shivappa@linux.intel.com>
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>

--- arch/x86/include/asm/intel_rdt.h
+++ arch/x86/include/asm/intel_rdt.h
@@ -3,14 +3,50 @@
 
 #ifdef CONFIG_INTEL_RDT
 
+#include <linux/jump_label.h>
+
 #define MAX_CBM_LENGTH			32
 #define IA32_L3_CBM_BASE		0xc90
 #define CBM_FROM_INDEX(x)		(IA32_L3_CBM_BASE + x)
+
+extern struct static_key rdt_enable_key;
+void __intel_rdt_sched_in(void *dummy);
 
 struct clos_cbm_table {
 	unsigned long cbm;
 	unsigned int clos_refcnt;
 };
+
+/*
+ * intel_rdt_sched_in() - Writes the task's CLOSid to IA32_PQR_MSR
+ *
+ * Following considerations are made so that this has minimal impact
+ * on scheduler hot path:
+ * - This will stay as no-op unless we are running on an Intel SKU
+ * which supports L3 cache allocation.
+ * - When support is present and enabled, does not do any
+ * IA32_PQR_MSR writes until the user starts really using the feature
+ * ie creates a rdtgroup directory and assigns a cache_mask thats
+ * different from the root rdtgroup's cache_mask.
+ * - Caches the per cpu CLOSid values and does the MSR write only
+ * when a task with a different CLOSid is scheduled in. That
+ * means the task belongs to a different rdtgroup.
+ * - Closids are allocated so that different rdtgroup directories
+ * with same cache_mask gets the same CLOSid. This minimizes CLOSids
+ * used and reduces MSR write frequency.
+ */
+static inline void intel_rdt_sched_in(void)
+{
+	/*
+	 * Call the schedule in code only when RDT is enabled.
+	 */
+	if (static_key_false(&rdt_enable_key))
+		__intel_rdt_sched_in(NULL);
+}
+
+#else
+
+static inline void intel_rdt_sched_in(void) {}
 
 #endif
 #endif
