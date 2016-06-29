be8c8f09647cb39c627833050d3adae7b7631721
Fenghua Yu <fenghua.yu@intel.com>
2016-06-29 13:31:28 -0700

    include/linux/resctrl.h: Define fork and exit functions in a new header file
    A new header file is created in include/linux/resctrl.h. It contains
    defintions of rdtgroup_fork and rdtgroup_exit for x86. The functions
    are empty for other architectures.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>

---
+++ include/linux/resctrl.h
@@ -0,0 +1,12 @@
+#ifndef _LINUX_RESCTRL_H
+#define _LINUX_RESCTRL_H
+
+#ifdef CONFIG_INTEL_RDT
+extern void rdtgroup_fork(struct task_struct *child);
+extern void rdtgroup_exit(struct task_struct *tsk);
+#else
+static inline void rdtgroup_fork(struct task_struct *child) {}
+static inline void rdtgroup_exit(struct task_struct *tsk) {}
+#endif /* CONFIG_X86 */
+
+#endif /* _LINUX_RESCTRL_H */
